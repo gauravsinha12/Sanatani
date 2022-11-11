fdc355254445fc0ff02b9b906ae95444952a0c3f
shreyanshgithubjain <shreyanshsomiljain@gmail.com>
2022-11-11 22:06:04 +0530

    s1

--- brahm_daan/lib/main.dart
+++ brahm_daan/lib/main.dart
@@ -3,7 +3,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'screens/login_page.dart';
 
-void main() async{
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
   runApp(const MyApp());
@@ -15,8 +15,32 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: MainPage(),
-    );
+        home: Scaffold(
+            appBar: AppBar(
+                title: Text('BramhDan'),
+                centerTitle: true,
+                leading: IconButton(
+                  icon: Icon(Icons.menu),
+                  onPressed: () {},
+                ),
+                actions: [
+                  IconButton(
+                    icon: Icon(Icons.notifications_none),
+                    onPressed: () {},
+                  ),
+                  IconButton(
+                    icon: Icon(Icons.search),
+                    onPressed: () {},
+                  )
+                ],
+                backgroundColor: Colors.blueAccent,
+                elevation: 0,
+                flexibleSpace: Container(
+                  decoration: BoxDecoration(
+                    image: DecorationImage(
+                        image: NetworkImage(
+                            'https://img.traveltriangle.com/blog/wp-content/uploads/2020/01/temples-in-India_8th-jan.jpg')),
+                  ),
+                ))));
   }
 }
